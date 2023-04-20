8f9794a51b015dafb0fee96e1646e4ed7e19b478
Daniel <danielmtdc16@gmail.com>
2023-04-20 18:05:54 -0300

    função para obter imagem.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:opencv_4/opencv_4.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,6 +12,15 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  final _picker = ImagePicker();
+  File? _image;
+
+  Future<void> _getImage(ImageSource source) async{
+    final pickedFile = await _picker.pickImage(source: source);
+    _image = pickedFile != null ? File(pickedFile.path) : null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,7 +28,13 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
         title: const Text("opencv"),
       ),
-      body: Container(),
+      body: const SingleChildScrollView(
+        child: Column(
+          children: [
+
+          ],
+        ),
+      ),
     );
   }
 }
